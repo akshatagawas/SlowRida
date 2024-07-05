11b0ba7705d729e3f129fa2b265a608fb32a7619
agawas <agawas@iu.edu>
2024-07-05 16:43:20 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export function App() {
+  const [redLight, setRedLight] = useState(true);
+  const [yellowLight, setYellowLight] = useState(false);
+  const [greenLight, setGreenLight] = useState(false);
+
+  useEffect(() => {
+    const trafficInterval = setInterval(() => {
+      setRedLight(true);
+      setYellowLight(false);
+      setGreenLight(false);
+
+      setTimeout(() => {
+        setRedLight(false);
+        setYellowLight(true);
+        setGreenLight(false);
+      }, 3000);
+
+      setTimeout(() => {
+        setRedLight(false);
+        setYellowLight(false);
+        setGreenLight(true);
+      }, 4000); 
+
+    }, 6000);
+
+    return () => clearInterval(trafficInterval)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Traffic Lights</h1>
+      <div className={(redLight? "red-circle" : "circle-hidden")+" circle"}></div>
+      <div className={(yellowLight? "yellow-circle" : "circle-hidden")+" circle"}></div>
+      <div className={(greenLight? "green-circle" : "circle-hidden")+" circle"}></div>
     </div>
   );
 }
